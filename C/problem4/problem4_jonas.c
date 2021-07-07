956266007c27ff3b43acc1e348ec7b3a7ac5cb71
Jonas Schmitt <jonas.schmitt@fau.de>
2021-07-07 15:58:06 +0200

    Problem 4: Use original version for new problem generation (without file name)

--- C/problem4/problem4_jonas.c
+++ C/problem4/problem4_jonas.c
@@ -16,17 +16,15 @@ static void init_matrix(double *matrix, int number_of_nodes) {
     }
 }
 
-struct problem *newProblem(const char *filename) {
+//struct problem *newProblem(const char *filename) {
+struct problem *newProblem(int n) {
     struct problem *p = NULL;
     //TODO get number of vertices
-    int n = 0;
     if (n > 0) {
         p = (struct problem *) malloc(sizeof (struct problem));
         p->n = n;
-        //TODO handle uneven numbers
         p->matrix = (double *)malloc(((n+1)*(n/2)-n) * sizeof(double));
         init_matrix(p->matrix, n);
-        //TODO initialize
     } else
         fprintf(stderr, "problem4: Invalid number of vertices: %d\n", n);
     return p;
