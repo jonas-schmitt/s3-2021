ac00c27d412457c0dc73c3e7071eae88d4019927
Noé Godinho <noe.godinho.92@gmail.com>
2021-07-07 10:47:27 +0100

    Updated file with defined methods in order to work on the compiler. Randint generates only positive values and free returns an error.

--- C/problem4/problem4.c
+++ C/problem4/problem4.c
@@ -48,17 +48,15 @@ static void init_matrix(double **edge_list, int number_of_nodes) {
 
 struct problem *newProblem(int n) {
     struct problem *p = NULL;
-    int i, j;
+    int i;
 
     if (n > 0) {
         p = (struct problem *) malloc(sizeof (struct problem));
         p->number_of_nodes = n;
-        p->edge_list = (double **) malloc(n * sizeof(double *)); // total size = ((n+1)*(n/2)-n)
+        p->edge_list = (double **) malloc(n * sizeof(double *)); // n rows, n-1-i columns, total size = ((n+1)*(n/2)-n)
 
         for(i = 0; i < n; ++i) {
-            for(j = i; j < n; ++j) {
-                p->edge_list[i] = (double *) malloc((n-1-i) * sizeof(double));
-            }
+            p->edge_list[i] = (double *) malloc((n-1-i) * sizeof(double));
         }
 
         init_matrix(p->edge_list, n);
@@ -95,21 +93,73 @@ void freeProblem(struct problem *p) {
 
 /* TODO */
 
-struct solution *allocSolution(struct problem *p);
-struct move *allocMove(struct problem *p);
+struct solution *allocSolution(struct problem *p){
+    return NULL;
+}
 
-void freeSolution(struct solution *s);
-void freeMove(struct move *v);
+struct move *allocMove(struct problem *p){
+    return NULL;
+}
+
+void freeSolution(struct solution *s){
+    return;
+}
+
+void freeMove(struct move *v){
+    return;
+}
+
+void printSolution(struct solution *s){
+    return;
+}
+
+void printMove(struct move *v){
+    return;
+}
 
 
-struct solution *randomSolution(struct solution *s);
-struct solution *copySolution(struct solution *dest, const struct solution *src);
-double *getObjectiveVector(double *objv, struct solution *s);
-struct solution *applyMove(struct solution *s, const struct move *v);
-int getNeighbourhoodSize(struct solution *s);
-struct solution *resetRandomMoveWOR(struct solution *s);
+struct solution *randomSolution(struct solution *s){
+    return NULL;
+}
 
-struct move *randomMove(struct move *v, const struct solution *s);
-struct move *copyMove(struct move *dest, const struct move *src);
-double *getObjectiveIncrement(double *obji, struct move *v, struct solution *s);
-struct move *randomMoveWOR(struct move *v, struct solution *s);
+struct solution *copySolution(struct solution *dest, const struct solution *src){
+    return NULL;
+}
+
+double *getObjectiveVector(double *objv, struct solution *s){
+    return NULL;
+}
+
+struct solution *applyMove(struct solution *s, const struct move *v){
+    return NULL;
+}
+
+int getNeighbourhoodSize(struct solution *s){
+    return 0;
+}
+
+struct solution *resetRandomMoveWOR(struct solution *s){
+    return NULL;
+}
+
+struct move *randomMove(struct move *v, const struct solution *s){
+    return NULL;
+}
+
+struct move *copyMove(struct move *dest, const struct move *src){
+    return NULL;
+}
+
+double *getObjectiveIncrement(double *obji, struct move *v, struct solution *s){
+    return NULL;
+}
+
+struct move *randomMoveWOR(struct move *v, struct solution *s){
+    return NULL;
+}
+
+
+/* Compiler says this is undefined, not sure if needed */
+struct solution *randomNeighbour(struct solution *s, int d){
+    return NULL;
+}
